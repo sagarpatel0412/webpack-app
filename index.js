759e47faacf2a2ee04a4f7a6fa1730885725eb0f
sagarpatel0412 <sagar.56001@gmail.com>
2024-07-27 18:35:40 +0530

    made some changes

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 console.log('hello')
+
+const app = {
+  myClick() {
+    console.log("hello");
+  },
+  app:{
+    name:"sagar"
+  },
+  
+};
+
+
+window.greece=app
+window.onload = function () {
+  document.getElementById("greeceAppName").textContent = greece.app.name;
+};
